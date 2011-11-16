b445f7d7c3ab4728460cb7b876c696ac7755a0d7
Alan Malloy <alan@malloys.org>
2011-11-16 00:15:16 -0800

    Update useful, use lein

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject snusp "0.0.1"
   :description "TODO: add summary of your project"
   :dependencies [[clojure "1.2.1"]
-                 [useful "0.7.5-alpha1"]])
+                 [useful "0.7.5-alpha2"]]
+  :checksum-deps true)
